5d26b0c72a058c2dac6dc070ab173b77e6f4575e
ch-anuj <an12ch98@gmail.com>
2016-06-18 21:38:27 +0530

    Add files via upload

--- new.js
+++ new.js
@@ -4,9 +4,8 @@ $(".dropdown-button").dropdown({
       outDuration: 225,
       constrain_width: false, // Does not change width of dropdown to that of the activator
       hover: true, // Activate on hover
-      gutter: -25, // Spacing from edge
+      gutter: -10, // Spacing from edge
       belowOrigin: true, // Displays dropdown below the button
       alignment: 'left' // Displays dropdown with edge aligned to the left of button
     });
 });
-
